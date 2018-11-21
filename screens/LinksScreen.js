ad1e70e54a716b6af344bcc68db3c725869e6743
Vasiliy Kashapov <vasya_kashapov@meta.ua>
2018-11-21 13:59:45 +0200

    change linkscreen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,121 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { StyleSheet, Image, Text, View } from 'react-native';
+import { WebBrowser } from 'expo';
+import Touchable from 'react-native-platform-touchable';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: null,
   };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View>
+        <Text style={styles.optionsTitleText}>
+          Links
+        </Text>
+
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressSource}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Image
+                source={require('../assets/images/github-icon.png')}
+                resizeMode="contain"
+                fadeDuration={0}
+                style={{ width: 20, height: 20, marginTop: 1 }}
+              />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                See propject at GitHub
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+          background={Touchable.Ripple('#ccc', false)}
+          style={styles.option}
+          onPress={this._handlePressSlack}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Image
+                source={require('../assets/images/telegram-icon.png')}
+                fadeDuration={0}
+                style={{ width: 20, height: 20 }}
+              />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Join us on Telegram
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handlePressForums}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Image
+                source={require("../assets/images/linkedin-icon.png")}
+                fadeDuration={0}
+                style={{width: 20, height: 20 }}
+              />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+                Ask question to developer
+              </Text>
+            </View>
+          </View>
+        </Touchable>
+      </View>
     );
   }
+
+
+_handlePressSlack = () => {
+  WebBrowser.openBrowserAsync('https://t.me/front_end_developer');
+};
+
+_handlePressSource = () => {
+  WebBrowser.openBrowserAsync('https://github.com/ArtistHam/radio');
+};
+
+_handlePressForums = () => {
+  WebBrowser.openBrowserAsync('https://www.linkedin.com/in/kashapovvas/');
+};
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#00d4ff',
+  },
+  optionsTitleText: {
+    fontSize: 16,
+    marginHorizontal: '40%',
+    marginVertical: 10,
+  },
+  optionIconContainer: {
+    marginRight: 9,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
+  },
+  optionText: {
+    fontSize: 15,
+    marginTop: 1,
   },
 });

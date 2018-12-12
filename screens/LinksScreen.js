190ebd72dad40034e0f86551880cb9e0cc46200d
Vasiliy Kashapov <vasya_kashapov@meta.ua>
2018-12-12 17:48:39 +0200

    links

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,121 +1,123 @@
-import React from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
-import { WebBrowser } from 'expo';
-import Touchable from 'react-native-platform-touchable';
+import React from "react";
+import { StyleSheet, ScrollView, Image, Text, View } from "react-native";
+import { WebBrowser } from "expo";
+import Touchable from "react-native-platform-touchable";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: null,
+    header: null
   };
 
   render() {
     return (
-      <View>
-        <Text style={styles.optionsTitleText}>
-          Links
-        </Text>
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <Text style={styles.optionsTitleText}>Links</Text>
 
-        <Touchable
-          style={styles.option}
-          background={Touchable.Ripple('#ccc', false)}
-          onPress={this._handlePressSource}>
-          <View style={{ flexDirection: 'row' }}>
-            <View style={styles.optionIconContainer}>
-              <Image
-                source={require('../assets/images/github-icon.png')}
-                resizeMode="contain"
-                fadeDuration={0}
-                style={{ width: 20, height: 20, marginTop: 1 }}
-              />
+          <Touchable
+            style={styles.option}
+            background={Touchable.Ripple("#ccc", false)}
+            onPress={this._handlePressSource}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.optionIconContainer}>
+                <Image
+                  source={require("../assets/images/github-icon.png")}
+                  resizeMode="contain"
+                  fadeDuration={0}
+                  style={{ width: 20, height: 20, marginTop: 1 }}
+                />
+              </View>
+              <View style={styles.optionTextContainer}>
+                <Text style={styles.optionText}>See project at GitHub</Text>
+              </View>
             </View>
-            <View style={styles.optionTextContainer}>
-              <Text style={styles.optionText}>
-                See propject at GitHub
-              </Text>
-            </View>
-          </View>
-        </Touchable>
+          </Touchable>
 
-        <Touchable
-          background={Touchable.Ripple('#ccc', false)}
-          style={styles.option}
-          onPress={this._handlePressSlack}>
-          <View style={{ flexDirection: 'row' }}>
-            <View style={styles.optionIconContainer}>
-              <Image
-                source={require('../assets/images/telegram-icon.png')}
-                fadeDuration={0}
-                style={{ width: 20, height: 20 }}
-              />
+          <Touchable
+            background={Touchable.Ripple("#ccc", false)}
+            style={styles.option}
+            onPress={this._handlePressSlack}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.optionIconContainer}>
+                <Image
+                  source={require("../assets/images/telegram-icon.png")}
+                  fadeDuration={0}
+                  style={{ width: 20, height: 20 }}
+                />
+              </View>
+              <View style={styles.optionTextContainer}>
+                <Text style={styles.optionText}>Join us on Telegram</Text>
+              </View>
             </View>
-            <View style={styles.optionTextContainer}>
-              <Text style={styles.optionText}>
-                Join us on Telegram
-              </Text>
-            </View>
-          </View>
-        </Touchable>
+          </Touchable>
 
-        <Touchable
-          style={styles.option}
-          background={Touchable.Ripple('#ccc', false)}
-          onPress={this._handlePressForums}>
-          <View style={{ flexDirection: 'row' }}>
-            <View style={styles.optionIconContainer}>
-              <Image
-                source={require("../assets/images/linkedin-icon.png")}
-                fadeDuration={0}
-                style={{width: 20, height: 20 }}
-              />
+          <Touchable
+            style={styles.option}
+            background={Touchable.Ripple("#ccc", false)}
+            onPress={this._handlePressForums}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <View style={styles.optionIconContainer}>
+                <Image
+                  source={require("../assets/images/linkedin-icon.png")}
+                  fadeDuration={0}
+                  style={{ width: 20, height: 20 }}
+                />
+              </View>
+              <View style={styles.optionTextContainer}>
+                <Text style={styles.optionText}>Ask question to developer</Text>
+              </View>
             </View>
-            <View style={styles.optionTextContainer}>
-              <Text style={styles.optionText}>
-                Ask question to developer
-              </Text>
-            </View>
-          </View>
-        </Touchable>
+          </Touchable>
+        </ScrollView>
       </View>
     );
   }
 
+  _handlePressSlack = () => {
+    WebBrowser.openBrowserAsync("https://t.me/front_end_developer");
+  };
 
-_handlePressSlack = () => {
-  WebBrowser.openBrowserAsync('https://t.me/front_end_developer');
-};
+  _handlePressSource = () => {
+    WebBrowser.openBrowserAsync("https://github.com/ArtistHam/radio");
+  };
 
-_handlePressSource = () => {
-  WebBrowser.openBrowserAsync('https://github.com/ArtistHam/radio');
-};
-
-_handlePressForums = () => {
-  WebBrowser.openBrowserAsync('https://www.linkedin.com/in/kashapovvas/');
-};
+  _handlePressForums = () => {
+    WebBrowser.openBrowserAsync("https://www.linkedin.com/in/kashapovvas/");
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#00d4ff',
+    backgroundColor: "#00d4ff",
   },
   optionsTitleText: {
-    fontSize: 16,
-    marginHorizontal: '40%',
-    marginVertical: 10,
+    fontSize: 24,
+    marginHorizontal: "40%",
+    paddingVertical: 5
   },
   optionIconContainer: {
-    marginRight: 9,
+    marginRight: 9
+  },
+  contentContainer: {
+    marginTop: 20
   },
   option: {
-    backgroundColor: '#fdfdfd',
+    backgroundColor: "#fdfdfd",
+    marginVertical: 2,
     paddingHorizontal: 15,
     paddingVertical: 15,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#EDEDED',
+    borderBottomColor: "#EDEDED"
   },
   optionText: {
     fontSize: 15,
-    marginTop: 1,
-  },
+    marginTop: 1
+  }
 });
